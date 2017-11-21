435263c4318f397f56f6d535dcdd992eb886747d
Michal <238181@student.pwr.edu.pl>
2017-11-21 02:27:57 +0100

    Results are now written to files.

--- src/webspider/ParserManager.java
+++ src/webspider/ParserManager.java
@@ -9,9 +9,13 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -19,13 +23,19 @@ import java.util.concurrent.Executors;
 
 public class ParserManager{
 
+    private final long PARSING_TIMEOUT = 5000L;
+    private long freshContentTimestamp = 0;
+
     private final String[] validatorSchemes = {"http", "https"};
 
-    private HashSet<String> results = new HashSet<>();
+    private HashSet<String> links = new HashSet<>();
+    private HashSet<String> imgs = new HashSet<>();
 
     private static ParserManager instance;
     private static ExecutorService threadPool;
     private static UrlValidator validator;
+
+    private Thread freshContentWarden;
 
     public static ParserManager getInstance() {
         if(instance == null)
@@ -36,13 +46,55 @@ public class ParserManager{
     private ParserManager(){
         threadPool = Executors.newFixedThreadPool(4);
         validator = new UrlValidator(validatorSchemes);
+
+        freshContentWarden = new Thread(() -> {
+            while(true){
+                try {
+                    Thread.sleep(500L);
+                    if (System.currentTimeMillis() - freshContentTimestamp > 5000L) {
+                        System.out.println("No content updates since " + PARSING_TIMEOUT + "ms \nExiting");
+                        closingSequence();
+                    }
+
+                } catch (InterruptedException iex) {
+                    iex.printStackTrace();
+                }
+            }
+        });
     }
 
     public void scheduleParsing(String s){
-        if (!results.contains(s)) {
+        if(!freshContentWarden.isAlive())
+            freshContentWarden.start();
+        if (!links.contains(s)) {
             threadPool.execute(new DocumentParser(s));
+            freshContentTimestamp = System.currentTimeMillis();
         }
+        System.out.println("Scheduling parse of " + s);
 
+    }
+
+    private void closingSequence(){
+
+
+        writeCollectionToFile("links.txt", links);
+        System.out.println("Printed links to links.txt");
+
+        writeCollectionToFile("imgs.txt", imgs);
+        System.out.println("Printed imgs to imgs.txt");
+
+        Runtime.getRuntime().exit(0);
+    }
+
+    private void writeCollectionToFile(String fileName, Collection<String> col){
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(fileName)))) {
+            for(String s: col){
+                bw.write(s);
+                bw.newLine();
+            }
+        }catch(IOException ioex){
+            ioex.printStackTrace();
+        }
     }
 
     class DocumentParser implements Runnable{
@@ -60,14 +112,20 @@ public class ParserManager{
             try{
                 Document doc = getDocumentFromUrl(url);
                 System.out.println("Getting doc");
-                Elements links = doc.select("a");
+                Elements aElems = doc.select("a");
 
-                for(Element el : links){
+                for(Element el : aElems){
                     String s = el.attr("href");
                     if (validator.isValid(s)) {
-                        results.add(s);
+                        ParserManager.this.links.add(s);
                         scheduleParsing(s);
                     }
+                }
+
+                Elements imgSrcs = doc.select("img");
+                for(Element el: imgSrcs){
+                    String s = el.attr("src");
+                    imgs.add(s);
                 }
             }catch(Exception ex){
                 ex.printStackTrace();
@@ -85,6 +143,10 @@ public class ParserManager{
         }
 
     }
+
+
+
+
 
 
 }
